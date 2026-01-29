7ce4a889f06fd34bb6e24ff8808a836bc112f737
Your Name <you@example.com>
2026-01-29 23:51:04 +0530

    user.jsx changes for feed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,16 +7,13 @@ const bcrypt = require("bcrypt");
 
 authRouter.post("/signup", async (req, res) => {
   try {
-    // Validation of data
     validateSignUpData(req);
 
     const { firstName, lastName, emailId, password } = req.body;
 
-    // Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
     console.log(passwordHash);
 
-    //   Creating a new instance of the User model
     const user = new User({
       firstName,
       lastName,
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,93 +1,92 @@
-const express = require('express');
+const express = require("express");
 const userRouter = express.Router();
-const User = require('../models/user');
 
-const { userAuth } = require('../middlewares/auth');
-const ConnectionRequest = require('../models/connectionRequest');
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-const USER_SAFE_DATA = ["_id", "firstName", "lastName", "emailId", "age", "photoUrl", "about", "skills"];
-//Get all the pending connection request for the logged in user
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
+// Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-    try{
-        const loggedIn = req.user;
-        const connectionRequests = await ConnectionRequest.find({   
-            toUserId: loggedIn._id,
-            status : "interested",
-        }).populate("fromUserId", USER_SAFE_DATA);   
+    const connectionRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", USER_SAFE_DATA);
+    // }).populate("fromUserId", ["firstName", "lastName"]);
 
-        res.json({
-            message: "Connection requests fetched successfully!",
-            data: connectionRequests,
-        });
-    }catch(err){
-        res.status(400).send("ERROR "+ err.message);
-    }
+    res.json({
+      message: "Data fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    req.statusCode(400).send("ERROR: " + err.message);
+  }
 });
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-    try{        
-        const loggedIn = req.user;
-        const connectionRequests = await ConnectionRequest.find({
-            $or: [
-                {fromUserId: loggedIn._id, status: "accepted"},
-                {toUserId: loggedIn._id, status: "accepted"},
-            ],
-        }).populate("fromUserId toUserId", USER_SAFE_DATA);
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
+    console.log(connectionRequests);
 
-        const data = connectionRequests.map((row) => {
-            if(row.fromUserId._id.toString() === loggedIn._id.toString()){
-                return row.toUserId;
-            }
-            return row.fromUserId;                       
-        });
-        res.json({
-            message: "Connections fetched successfully!",
-            data,
-        });
-        }catch(err){
-        res.status(400).send("ERROR "+ err.message);
-    }
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
 });
 
 userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-    try{    
-        const loggedIn = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
-        const page = parseInt(req.query.page) || 1;
-        let limit = parseInt(req.query.limit) || 10;
-        limit=limit>50?50:limit;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId  toUserId");
 
-        const skip = (page - 1) * limit;
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
 
-        const connectionRequests = await ConnectionRequest.find({
-            $or: [
-                {fromUserId: loggedIn._id},
-                {toUserId: loggedIn._id},
-            ],
-        }).select("fromUserId toUserId");
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
-        const excludedUserIds = new Set();
-        connectionRequests.forEach((req) => {
-            excludedUserIds.add(req.fromUserId.toString());
-            excludedUserIds.add(req.toUserId.toString());
-        });
-
-        const users = await User.find({
-            $and : [
-                {_id: { $nin: Array.from(excludedUserIds) }},
-                {_id: {$ne : loggedIn._id}} 
-            ]
-        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
-
-        res.json({data : users});
-
-    }catch(err){
-        res.status(400).send("ERROR "+ err.message);
-    }
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
-
 module.exports = userRouter;
